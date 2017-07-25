8c7b5e956475e24ae1142b85c2229231f4c015a5
staryash <staryash@gmail.com>
2017-07-25 16:36:13 +0530

    model box on fix click

--- src/renderer/components/Integrate.vue
+++ src/renderer/components/Integrate.vue
@@ -53,10 +53,6 @@
       <button class="ui large orange button right floated" @click="beginValidateProject">Rescan files</button>
     </div>
 
-    <div class="right floated six wide column" v-if="state == 'review-files'">
-      <button class="ui large primary button right floated" @click="reviewUpdates">Review Inputs</button>
-      <button class="ui large orange button right floated" @click="state = 'scanned-files'">Back</button>
-    </div>
 
     <div class="sixteen wide column" v-if="state == 'review-files'">
       <div class="ui styled fluid accordion" style="overflow-y: auto; max-height: 53vh;">
@@ -110,8 +106,11 @@
           </div>
         </template>
       </div>
+    </div>
 
-
+    <div class="right floated six wide column" v-if="state == 'review-files'">
+      <button class="ui large primary button right floated" @click="reviewUpdates">Review Inputs</button>
+      <button class="ui large orange button right floated" @click="state = 'scanned-files'">Back</button>
     </div>
 
 
@@ -142,7 +141,7 @@
 
       <h1>Results</h1>
 
-      <div class="ui styled fluid accordion">
+      <div class="ui styled fluid accordion" style="overflow-y: auto; max-height: 53vh;">
         <template v-for="liveChange in liveChanges">
 
           <div class="title active">
